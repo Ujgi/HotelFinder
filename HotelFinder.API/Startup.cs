25d75c10222c1d973192d895c0783609fab1fdfc
Ujgi <utku.seker1021@gmail.com>
2022-08-23 00:17:48 +0300

    HotelFinder bitti gibimsi

---
+++ HotelFinder.API/Startup.cs
@@ -0,0 +1,37 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.Business.Concrete;
+using HotelFinder.DataAccess.Abstract;
+using HotelFinder.DataAccess.Concrete;
+
+namespace HotelFinder.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public void IConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSingleton<IHotelService, HotelManager>();
+            services.AddSingleton<IHotelRepository, HotelRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
+
+
